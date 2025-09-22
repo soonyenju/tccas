5f4f343325cd6fc9667666ba05852834b7e39e4d
Songyan Zhu <zhusy93@gmail.com>
2025-09-22 05:57:19 +0200

    Create __init__.py

---
+++ tccas/__init__.py
@@ -0,0 +1,10 @@
+# from . import gapfilling
+# from .gapfilling import *
+# from . import partitioning
+# from .partitioning import *
+# from .ustar_filtering import ustarfilter
+# from . import partitioning2
+# from . import eddyflux
+# from . import bigleaf
+
+__all__ = ["env_setup", "functions"]
