e8bf114d1761a9ab2363f0b726d765c61025565a
Songyan Zhu <zhusy93@gmail.com>
2025-09-22 07:20:21 +0200

    Update env_setup.py

--- tccas/env_setup.py
+++ tccas/env_setup.py
@@ -19,11 +19,11 @@ def config_netcdf():
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-def prepare_tccas():
+def prepare_tccas(home_dir):
     # Define paths
     source_file = Path("/content/drive/My Drive/tccas/bak/tccas_r10043.tgz")
-    dest_file = Path("/content/tccas_r10043.tgz")
-    extract_dir = Path("/content/tccas_r10043")
+    dest_file = home_dir.joinpath("tccas_r10043.tgz")
+    extract_dir = home_dir.joinpath("tccas_r10043")
 
     # 1. Remove old copy if exists
     if dest_file.exists():
@@ -54,11 +54,11 @@ warnings.simplefilter('ignore')
 
 drive.mount('/content/drive', force_remount = False)
 # home_dir = Path('/content')
-root = Path.cwd().joinpath('drive/My Drive/tccas')
+home_dir = Path.cwd().joinpath('drive/My Drive/tccas')
 # home_dir = [p for p in Path('/home').glob('*') if p.stem != 'conda'][0]
 os.chdir(home_dir)
 config_netcdf()
-prepare_tccas()
+prepare_tccas(home_dir)
 root_proj = home_dir.joinpath('tccas_r10043')
 sys.path.append(root_proj.parent.joinpath('notebooks').as_posix())
 sys.path.append(root_proj.parent.joinpath('notebooks_main_dev').as_posix())
